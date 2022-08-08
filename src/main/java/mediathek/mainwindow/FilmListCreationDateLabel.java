1373b3f3263fd6607832c1ab0c3c1f2f2f129880
Christian F <crystalpalace1977@googlemail.com>
2022-08-08 17:17:44 +0200

    - fix incorrect list creation date

--- src/main/java/mediathek/mainwindow/FilmListCreationDateLabel.java
+++ src/main/java/mediathek/mainwindow/FilmListCreationDateLabel.java
@@ -2,6 +2,9 @@ package mediathek.mainwindow;
 
 import mediathek.config.Daten;
 import mediathek.daten.FilmListMetaData;
+import mediathek.gui.messages.FilmListReadStopEvent;
+import mediathek.tool.MessageBus;
+import net.engio.mbassy.listener.Handler;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
@@ -14,11 +17,18 @@ public class FilmListCreationDateLabel extends JLabel implements PropertyChangeL
         setText(blacklist.getMetaData());
         //works only on blacklist!
         blacklist.addMetaDataChangeListener(this);
+
+        MessageBus.getMessageBus().subscribe(this);
     }
 
     private void setText(@NotNull FilmListMetaData metaData) {
         var text = String.format("Filmliste erstellt: %s Uhr", metaData.getGenerationDateTimeAsString());
         SwingUtilities.invokeLater(() -> setText(text));
+    }
+
+    @Handler
+    private void handleFilmListStop(@NotNull FilmListReadStopEvent e) {
+        setText(Daten.getInstance().getListeFilmeNachBlackList().getMetaData());
     }
 
     @Override
