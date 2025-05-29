c2091818ee74faf18389ab639bafc1fa8adfb864
Christian F. <crystalpalace1977@googlemail.com>
2025-05-29 17:29:04 +0200

    - remove expiry data

--- src/main/java/mediathek/javafx/bookmark/BookmarkNoteDialogController.java
+++ src/main/java/mediathek/javafx/bookmark/BookmarkNoteDialogController.java
@@ -6,8 +6,6 @@ import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 import java.net.URL;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.ResourceBundle;
 
 
@@ -20,10 +18,6 @@ import java.util.ResourceBundle;
  */
 
 public class BookmarkNoteDialogController implements Initializable {
-    /**
-     * Try to retrieve the expiry date from the associated webpage
-     */
-    private final DateTimeFormatter dateformatter;
     @FXML
     protected Button SaveButton;
     @FXML
@@ -31,27 +25,15 @@ public class BookmarkNoteDialogController implements Initializable {
     protected Stage dlgstage;
     protected boolean datachanged;
     @FXML
-    private DatePicker fxDate;
-    @FXML
     private TextArea fxNote;
     @FXML
     private ProgressIndicator fxProgress;
     @FXML
     private Label fxStatus;
-    @FXML
-    private Label fxExpiry;
     private BookmarkData data;
-
-    public BookmarkNoteDialogController() {
-        dateformatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    }
 
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
-        fxDate.setOnKeyTyped((var _) -> handleChange());
-        fxDate.setOnMouseClicked((var _) -> handleChange());
-        fxDate.getEditor().setOnKeyTyped((var _) -> handleChange());
-
         ButtonBar.setButtonData(CancelButton, ButtonBar.ButtonData.CANCEL_CLOSE);
         ButtonBar.setButtonData(SaveButton, ButtonBar.ButtonData.OK_DONE);
     }
@@ -72,58 +54,15 @@ public class BookmarkNoteDialogController implements Initializable {
         dlgstage.hide();
     }
 
-    @FXML
-    protected void handleChange() {
-        boolean isok = Verify();
-        SaveButton.setDisable(!isok);
-        int idx = fxDate.getEditor().getStyleClass().indexOf("Invalid");
-        if (isok && idx > -1) {
-            fxDate.getEditor().getStyleClass().remove("Invalid");
-        } else if (!isok && idx == -1) {
-            fxDate.getEditor().getStyleClass().add("Invalid");
-        }
-    }
-
     public final boolean SetandShow(Stage dlgstage, BookmarkData data) {
         this.dlgstage = dlgstage;
         this.data = data;
         this.dlgstage.setTitle(data.getNote() != null ? "Anmerkungen ändern" : "Neue Anmerkungen");
         fxNote.setText(data.getNote() != null ? data.getNote() : "");
-        if (data.isLiveStream()) { // For live stream disable expiry handling
-            fxExpiry.setDisable(true);
-            fxDate.setDisable(true);
-        }
-        handleChange();
+
+        SaveButton.setDisable(false);
         // Display the Dialog and wait
         this.dlgstage.showAndWait();
         return datachanged;
     }
-
-    protected boolean Verify() {
-        boolean rc = true;
-        // Check date format:
-        String dv = getDateValue();
-        if (dv != null) {
-            try {
-                LocalDate.parse(dv, dateformatter);
-            } catch (Exception e) {
-                rc = false;
-            }
-        }
-        return rc;
-    }
-
-    /**
-     * Get date value or null
-     *
-     * @return String
-     */
-    private String getDateValue() {
-        String dv = fxDate.getEditor().getText();
-        if (dv != null && dv.isEmpty()) {
-            dv = null;
-        }
-        return dv;
-    }
-
 }
