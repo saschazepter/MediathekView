b78f26d3ce1c19b347187c49abc07abafd16476f
Christian F. <crystalpalace1977@googlemail.com>
2025-06-28 15:33:48 +0200

    wip

--- src/main/java/mediathek/gui/bookmark/renderer/AvailableUntilCellRenderer.java
+++ src/main/java/mediathek/gui/bookmark/renderer/AvailableUntilCellRenderer.java
@@ -19,19 +19,58 @@
 package mediathek.gui.bookmark.renderer;
 
 import mediathek.tool.datum.DateUtil;
+import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 import java.awt.*;
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 public class AvailableUntilCellRenderer extends CenteredCellRenderer {
+    private static final long DAYS_UNTIL_END = 5;
+
     @Override
     public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
         super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+
         var date = (LocalDate) value;
+        var today = LocalDate.now();
         if (date != null) {
+            var dayDifference = Math.abs(ChronoUnit.DAYS.between(today, date));
+            if (date.isBefore(LocalDate.now())) {
+                setForeground(Color.red);
+            }
+            else if (dayDifference < DAYS_UNTIL_END) {
+                setForeground(Color.orange);
+            }
+            else {
+                setTextForeground(table, isSelected);
+            }
             setText(date.format(DateUtil.FORMATTER));
         }
         return this;
+    }
+
+    private boolean checkExpiry(LocalDate date) {
+        var today = LocalDate.now();
+        var dayDifference = Math.abs(ChronoUnit.DAYS.between(today, date));
+        if (date.isBefore(LocalDate.now())) {
+            setForeground(Color.red);
+            return true;
+        }
+        else if (dayDifference < DAYS_UNTIL_END) {
+            setForeground(Color.orange);
+            return true;
+        }
+        return false;
+    }
+
+    private void setTextForeground(@NotNull JTable table, boolean isSelected) {
+        if (isSelected) {
+            setForeground(table.getSelectionForeground());
+        }
+        else {
+            setForeground(table.getForeground());
+        }
     }
 }
