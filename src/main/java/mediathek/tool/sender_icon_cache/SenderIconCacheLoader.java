6dcbb175ab3ad3b391775eabd395c056bfc79606
Christian F. <crystalpalace1977@googlemail.com>
2026-03-07 18:01:43 +0100

    - add logging to sender icon cache loader

--- src/main/java/mediathek/tool/sender_icon_cache/SenderIconCacheLoader.java
+++ src/main/java/mediathek/tool/sender_icon_cache/SenderIconCacheLoader.java
@@ -39,7 +39,7 @@ class SenderIconCacheLoader {
      * @param localResource   resource address
      * @return the scaled image
      */
-    private @Nullable ImageIcon getIcon(@NotNull String networkResource, @NotNull String localResource) {
+    private @Nullable ImageIcon getIcon(@NotNull String sender, @NotNull String networkResource, @NotNull String localResource) {
         ImageIcon icon = null;
 
         if (!useLocalIcons.get()) {
@@ -55,8 +55,13 @@ class SenderIconCacheLoader {
                 if (response.isSuccessful()) {
                     BufferedImage b_img = ImageIO.read(body.byteStream());
                     icon = new ImageIcon(b_img);
+                } else {
+                    logger.warn("Wiki icon request failed for sender '{}' (url: {}, status: {})",
+                            sender, networkResource, response.code());
                 }
             } catch (Exception ex) {
+                logger.warn("Wiki icon request failed for sender '{}' (url: {})",
+                        sender, networkResource, ex);
                 icon = null;
             }
         }
@@ -67,9 +72,14 @@ class SenderIconCacheLoader {
             if (url != null)
                 icon = new ImageIcon(url);
             else {
-                logger.error("Could not load icon from local jar");
+                logger.error("Could not load icon from local jar for sender '{}' (resource: {})",
+                        sender, localResource);
                 icon = null;
             }
+        }
+
+        if (!useLocalIcons.get() && icon == null) {
+            logger.warn("Wiki icon result is empty for sender '{}' (url: {})", sender, networkResource);
         }
 
         return icon;
@@ -77,37 +87,37 @@ class SenderIconCacheLoader {
 
     public @NotNull Optional<ImageIcon> load(@NotNull String sender) {
         ImageIcon icon = switch (sender) {
-            case "3Sat" -> getIcon(WIKI_BASE_URL + "/f/f2/3sat-Logo.svg/775px-3sat-Logo.svg.png", "/mediathek/res/sender/3sat.png");
-            case "ARD" -> getIcon(WIKI_BASE_URL + "/6/68/ARD_logo.svg/320px-ARD_logo.svg.png", "/mediathek/res/sender/ard.png");
+            case "3Sat" -> getIcon("3Sat", WIKI_BASE_URL + "/f/f2/3sat-Logo.svg/775px-3sat-Logo.svg.png", "/mediathek/res/sender/3sat.png");
+            case "ARD" -> getIcon("ARD", WIKI_BASE_URL + "/6/68/ARD_logo.svg/320px-ARD_logo.svg.png", "/mediathek/res/sender/ard.png");
             //case "ARD-alpha" -> null;
-            case "ARTE.DE" -> getIcon(WIKI_BASE_URL + "/0/0e/Arte_Logo_2011.svg/320px-Arte_Logo_2011.svg.png", "/mediathek/res/sender/arte-de.png");
-            case "ARTE.EN" -> getIcon(WIKI_BASE_URL + "/0/0e/Arte_Logo_2011.svg/320px-Arte_Logo_2011.svg.png", "/mediathek/res/sender/arte-en.png");
-            case "ARTE.ES" -> getIcon(WIKI_BASE_URL + "/0/0e/Arte_Logo_2011.svg/320px-Arte_Logo_2011.svg.png", "/mediathek/res/sender/arte-es.png");
-            case "ARTE.FR" -> getIcon(WIKI_BASE_URL + "/0/0e/Arte_Logo_2011.svg/320px-Arte_Logo_2011.svg.png", "/mediathek/res/sender/arte-fr.png");
-            case "ARTE.IT" -> getIcon(WIKI_BASE_URL + "/0/0e/Arte_Logo_2011.svg/320px-Arte_Logo_2011.svg.png", "/mediathek/res/sender/arte-it.png");
-            case "ARTE.PL" -> getIcon(WIKI_BASE_URL + "/0/0e/Arte_Logo_2011.svg/320px-Arte_Logo_2011.svg.png", "/mediathek/res/sender/arte-pl.png");
-            case "BR" -> getIcon(WIKI_BASE_URL + "/9/98/BR_Dachmarke.svg/320px-BR_Dachmarke.svg.png", "/mediathek/res/sender/br.png");
-            case "HR" -> getIcon(WIKI_BASE_URL + "/6/63/HR_Logo.svg/519px-HR_Logo.svg.png", "/mediathek/res/sender/hr.png");
-            case "KiKA" -> getIcon(WIKI_BASE_URL + "/f/f5/Kika_2012.svg/320px-Kika_2012.svg.png", "/mediathek/res/sender/kika.png");
-            case "MDR" -> getIcon(WIKI_BASE_URL + "/6/61/MDR_Logo_2017.svg/800px-MDR_Logo_2017.svg.png", "/mediathek/res/sender/mdr.png");
-            case "DW" -> getIcon(WIKI_BASE_URL + "/6/69/Deutsche_Welle_Logo.svg/743px-Deutsche_Welle_Logo.svg.png", "/mediathek/res/sender/dw.png");
-            case "NDR" -> getIcon(WIKI_BASE_URL + "/0/08/NDR_Dachmarke.svg/308px-NDR_Dachmarke.svg.png", "/mediathek/res/sender/ndr.png");
+            case "ARTE.DE" -> getIcon("ARTE.DE", WIKI_BASE_URL + "/0/0e/Arte_Logo_2011.svg/320px-Arte_Logo_2011.svg.png", "/mediathek/res/sender/arte-de.png");
+            case "ARTE.EN" -> getIcon("ARTE.EN", WIKI_BASE_URL + "/0/0e/Arte_Logo_2011.svg/320px-Arte_Logo_2011.svg.png", "/mediathek/res/sender/arte-en.png");
+            case "ARTE.ES" -> getIcon("ARTE.ES", WIKI_BASE_URL + "/0/0e/Arte_Logo_2011.svg/320px-Arte_Logo_2011.svg.png", "/mediathek/res/sender/arte-es.png");
+            case "ARTE.FR" -> getIcon("ARTE.FR", WIKI_BASE_URL + "/0/0e/Arte_Logo_2011.svg/320px-Arte_Logo_2011.svg.png", "/mediathek/res/sender/arte-fr.png");
+            case "ARTE.IT" -> getIcon("ARTE.IT", WIKI_BASE_URL + "/0/0e/Arte_Logo_2011.svg/320px-Arte_Logo_2011.svg.png", "/mediathek/res/sender/arte-it.png");
+            case "ARTE.PL" -> getIcon("ARTE.PL", WIKI_BASE_URL + "/0/0e/Arte_Logo_2011.svg/320px-Arte_Logo_2011.svg.png", "/mediathek/res/sender/arte-pl.png");
+            case "BR" -> getIcon("BR", WIKI_BASE_URL + "/9/98/BR_Dachmarke.svg/320px-BR_Dachmarke.svg.png", "/mediathek/res/sender/br.png");
+            case "HR" -> getIcon("HR", WIKI_BASE_URL + "/6/63/HR_Logo.svg/519px-HR_Logo.svg.png", "/mediathek/res/sender/hr.png");
+            case "KiKA" -> getIcon("KiKA", WIKI_BASE_URL + "/f/f5/Kika_2012.svg/320px-Kika_2012.svg.png", "/mediathek/res/sender/kika.png");
+            case "MDR" -> getIcon("MDR", WIKI_BASE_URL + "/6/61/MDR_Logo_2017.svg/800px-MDR_Logo_2017.svg.png", "/mediathek/res/sender/mdr.png");
+            case "DW" -> getIcon("DW", WIKI_BASE_URL + "/6/69/Deutsche_Welle_Logo.svg/743px-Deutsche_Welle_Logo.svg.png", "/mediathek/res/sender/dw.png");
+            case "NDR" -> getIcon("NDR", WIKI_BASE_URL + "/0/08/NDR_Dachmarke.svg/308px-NDR_Dachmarke.svg.png", "/mediathek/res/sender/ndr.png");
             //case "ONE" -> null;
             //case "tagesschau24" -> null;
-            case "ORF" -> getIcon(WIKI_BASE_URL + "/d/dd/ORF_logo.svg/709px-ORF_logo.svg.png", "/mediathek/res/sender/orf.png");
-            case "RBB" -> getIcon(WIKI_BASE_URL + "/7/79/Rbb_Logo_2017.08.svg/320px-Rbb_Logo_2017.08.svg.png", "/mediathek/res/sender/rbb.png");
-            case "SR" -> getIcon(WIKI_BASE_URL + "/8/83/SR_Dachmarke.svg/602px-SR_Dachmarke.svg.png", "/mediathek/res/sender/sr.png");
-            case "SRF" -> getIcon(WIKI_BASE_URL + "/8/84/Schweizer_Radio_und_Fernsehen_Logo.svg/559px-Schweizer_Radio_und_Fernsehen_Logo.svg.png", "/mediathek/res/sender/srf.png");
+            case "ORF" -> getIcon("ORF", WIKI_BASE_URL + "/d/dd/ORF_logo.svg/709px-ORF_logo.svg.png", "/mediathek/res/sender/orf.png");
+            case "RBB" -> getIcon("RBB", WIKI_BASE_URL + "/7/79/Rbb_Logo_2017.08.svg/320px-Rbb_Logo_2017.08.svg.png", "/mediathek/res/sender/rbb.png");
+            case "SR" -> getIcon("SR", WIKI_BASE_URL + "/8/83/SR_Dachmarke.svg/602px-SR_Dachmarke.svg.png", "/mediathek/res/sender/sr.png");
+            case "SRF" -> getIcon("SRF", WIKI_BASE_URL + "/8/84/Schweizer_Radio_und_Fernsehen_Logo.svg/559px-Schweizer_Radio_und_Fernsehen_Logo.svg.png", "/mediathek/res/sender/srf.png");
             //case "SRF.Podcast" -> null;
-            case "SWR" -> getIcon(WIKI_BASE_URL + "/6/6f/SWR_Dachmarke.svg/320px-SWR_Dachmarke.svg.png", "/mediathek/res/sender/swr.png");
-            case "WDR" -> getIcon(WIKI_BASE_URL + "/9/9b/WDR_Dachmarke.svg/320px-WDR_Dachmarke.svg.png", "/mediathek/res/sender/wdr.png");
-            case "ZDF" -> getIcon(WIKI_BASE_URL + "/c/c1/ZDF_logo.svg/200px-ZDF_logo.svg.png", "/mediathek/res/sender/zdf.png");
+            case "SWR" -> getIcon("SWR", WIKI_BASE_URL + "/6/6f/SWR_Dachmarke.svg/320px-SWR_Dachmarke.svg.png", "/mediathek/res/sender/swr.png");
+            case "WDR" -> getIcon("WDR", WIKI_BASE_URL + "/9/9b/WDR_Dachmarke.svg/320px-WDR_Dachmarke.svg.png", "/mediathek/res/sender/wdr.png");
+            case "ZDF" -> getIcon("ZDF", WIKI_BASE_URL + "/c/c1/ZDF_logo.svg/200px-ZDF_logo.svg.png", "/mediathek/res/sender/zdf.png");
             //case "ZDFinfo" -> null;
             //case "ZDFneo" -> null;
             case "ZDF-tivi" -> getLocalImageIcon("/mediathek/res/sender/zdf-tivi.png");
-            case "PHOENIX" -> getIcon(WIKI_BASE_URL + "/d/de/Phoenix_Logo_2018_ohne_Claim.svg/640px-Phoenix_Logo_2018_ohne_Claim.svg.png", "/mediathek/res/sender/phoenix.png");
-            case "Funk.net" -> getIcon(WIKI_BASE_URL + "/9/99/Funk_Logo.svg/454px-Funk_Logo.svg.png", "/mediathek/res/sender/funk_net.png");
-            case "Radio Bremen TV" -> getIcon(WIKI_BASE_URL + "/7/73/Logo_Radio_Bremen_TV.svg/320px-Logo_Radio_Bremen_TV.svg.png", "/mediathek/res/sender/rbtv.jpg");
+            case "PHOENIX" -> getIcon("PHOENIX", WIKI_BASE_URL + "/d/de/Phoenix_Logo_2018_ohne_Claim.svg/640px-Phoenix_Logo_2018_ohne_Claim.svg.png", "/mediathek/res/sender/phoenix.png");
+            case "Funk.net" -> getIcon("Funk.net", WIKI_BASE_URL + "/9/99/Funk_Logo.svg/454px-Funk_Logo.svg.png", "/mediathek/res/sender/funk_net.png");
+            case "Radio Bremen TV" -> getIcon("Radio Bremen TV", WIKI_BASE_URL + "/7/73/Logo_Radio_Bremen_TV.svg/320px-Logo_Radio_Bremen_TV.svg.png", "/mediathek/res/sender/rbtv.jpg");
             default -> null;
         };
 
