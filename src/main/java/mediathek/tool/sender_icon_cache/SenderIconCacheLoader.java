753b63480e0c12cebc73327040d77320e3f8f0dc
Christian F. <crystalpalace1977@googlemail.com>
2026-03-07 18:11:37 +0100

    - use the local sender icons for cell render display

--- src/main/java/mediathek/tool/sender_icon_cache/SenderIconCacheLoader.java
+++ src/main/java/mediathek/tool/sender_icon_cache/SenderIconCacheLoader.java
@@ -1,4 +1,6 @@
 package mediathek.tool.sender_icon_cache;
+
+import com.formdev.flatlaf.extras.FlatSVGIcon;
 import mediathek.config.Konstanten;
 import mediathek.tool.http.MVHttpClient;
 import okhttp3.Request;
@@ -11,7 +13,9 @@ import org.jetbrains.annotations.Nullable;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.util.Locale;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicBoolean;
 
@@ -32,6 +36,92 @@ class SenderIconCacheLoader {
             return null;
     }
 
+    private @Nullable ImageIcon getBundledSenderIcon(@NotNull String sender) {
+        String resource = switch (sender.toLowerCase(Locale.ROOT)) {
+            case "3sat" -> "/icons/sender/3sat.svg";
+            case "ard", "das erste" -> "/icons/sender/ard.svg";
+            case "ard-alpha" -> "/icons/sender/ard-alpha.svg";
+            case "arte.de", "arte.en", "arte.es", "arte.fr", "arte.it", "arte.pl", "arte" -> "/icons/sender/arte.svg";
+            case "br" -> "/icons/sender/br.svg";
+            case "hr" -> "/icons/sender/hr.svg";
+            case "kika" -> "/icons/sender/kika.svg";
+            case "mdr" -> "/icons/sender/mdr.svg";
+            case "ndr" -> "/icons/sender/ndr.svg";
+            case "one" -> "/icons/sender/one.svg";
+            case "phoenix" -> "/icons/sender/phoenix.svg";
+            case "radio bremen tv", "radio bremen" -> "/icons/sender/radio-bremen.svg";
+            case "rbb" -> "/icons/sender/rbb.svg";
+            case "sr" -> "/icons/sender/sr.svg";
+            case "swr" -> "/icons/sender/swr.svg";
+            case "tagesschau24" -> "/icons/sender/tagesschau24.svg";
+            case "wdr" -> "/icons/sender/wdr.svg";
+            case "zdf" -> "/icons/sender/zdf.svg";
+            case "zdfinfo" -> "/icons/sender/ZDFinfo.svg";
+            case "zdfneo" -> "/icons/sender/ZDFneo.svg";
+            case "deutscher bundestag", "parlamentsfernsehen kanal 1", "parlamentsfernsehen kanal 2" -> "/icons/sender/Deutscher_Bundestag.svg";
+            default -> null;
+        };
+
+        if (resource == null) {
+            return null;
+        }
+
+        var url = SenderIconCacheLoader.class.getResource(resource);
+        if (url == null) {
+            logger.warn("Bundled sender icon resource missing for sender '{}' (resource: {})", sender, resource);
+            return null;
+        }
+
+        if (resource.endsWith(".svg")) {
+            FlatSVGIcon svgIcon = new FlatSVGIcon(url).derive(256, 256);
+            BufferedImage rendered = new BufferedImage(svgIcon.getIconWidth(), svgIcon.getIconHeight(), BufferedImage.TYPE_INT_ARGB);
+            Graphics2D g2 = rendered.createGraphics();
+            try {
+                svgIcon.paintIcon(null, g2, 0, 0);
+            } finally {
+                g2.dispose();
+            }
+
+            return new ImageIcon(trimTransparentBorder(rendered));
+        }
+
+        return new ImageIcon(url);
+    }
+
+    private static @NotNull BufferedImage trimTransparentBorder(@NotNull BufferedImage image) {
+        int minX = image.getWidth();
+        int minY = image.getHeight();
+        int maxX = -1;
+        int maxY = -1;
+
+        for (int y = 0; y < image.getHeight(); y++) {
+            for (int x = 0; x < image.getWidth(); x++) {
+                int alpha = (image.getRGB(x, y) >>> 24) & 0xFF;
+                if (alpha != 0) {
+                    if (x < minX) minX = x;
+                    if (y < minY) minY = y;
+                    if (x > maxX) maxX = x;
+                    if (y > maxY) maxY = y;
+                }
+            }
+        }
+
+        if (maxX < minX || maxY < minY) {
+            return image;
+        }
+
+        int width = (maxX - minX) + 1;
+        int height = (maxY - minY) + 1;
+        BufferedImage cropped = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2 = cropped.createGraphics();
+        try {
+            g2.drawImage(image, 0, 0, width, height, minX, minY, maxX + 1, maxY + 1, null);
+        } finally {
+            g2.dispose();
+        }
+        return cropped;
+    }
+
     /**
      * Download an icon from network or use local resourcec
      *
@@ -41,6 +131,7 @@ class SenderIconCacheLoader {
      */
     private @Nullable ImageIcon getIcon(@NotNull String sender, @NotNull String networkResource, @NotNull String localResource) {
         ImageIcon icon = null;
+        final ImageIcon bundledSenderIcon = getBundledSenderIcon(sender);
 
         if (!useLocalIcons.get()) {
             var userAgent = String.format("%s %s", Konstanten.PROGRAMMNAME,Konstanten.MVVERSION);
@@ -68,13 +159,17 @@ class SenderIconCacheLoader {
 
         //if network is unreachable we get an image with size -1...
         if (icon == null || icon.getIconWidth() < 0 || icon.getIconHeight() < 0) {
-            var url = SenderIconCacheLoader.class.getResource(localResource);
-            if (url != null)
-                icon = new ImageIcon(url);
-            else {
-                logger.error("Could not load icon from local jar for sender '{}' (resource: {})",
-                        sender, localResource);
-                icon = null;
+            if (bundledSenderIcon != null) {
+                icon = bundledSenderIcon;
+            } else {
+                var url = SenderIconCacheLoader.class.getResource(localResource);
+                if (url != null)
+                    icon = new ImageIcon(url);
+                else {
+                    logger.error("Could not load icon from local jar for sender '{}' (resource: {}, bundled: none)",
+                            sender, localResource);
+                    icon = null;
+                }
             }
         }
 
@@ -118,7 +213,7 @@ class SenderIconCacheLoader {
             case "PHOENIX" -> getIcon("PHOENIX", WIKI_BASE_URL + "/d/de/Phoenix_Logo_2018_ohne_Claim.svg/640px-Phoenix_Logo_2018_ohne_Claim.svg.png", "/mediathek/res/sender/phoenix.png");
             case "Funk.net" -> getIcon("Funk.net", WIKI_BASE_URL + "/9/99/Funk_Logo.svg/454px-Funk_Logo.svg.png", "/mediathek/res/sender/funk_net.png");
             case "Radio Bremen TV" -> getIcon("Radio Bremen TV", WIKI_BASE_URL + "/7/73/Logo_Radio_Bremen_TV.svg/320px-Logo_Radio_Bremen_TV.svg.png", "/mediathek/res/sender/rbtv.jpg");
-            default -> null;
+            default -> getBundledSenderIcon(sender);
         };
 
         final Optional<ImageIcon> optIcon;
