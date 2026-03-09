d88d82408b1dfcf50f9a497229d94cd09b665eaf
Christian F. <crystalpalace1977@googlemail.com>
2026-03-09 07:49:28 +0100

    include git build information into the app

--- src/main/java/mediathek/gui/dialog/AboutDialog.java
+++ src/main/java/mediathek/gui/dialog/AboutDialog.java
@@ -1,5 +1,24 @@
+/*
+ * Copyright (c) 2026 derreisende77.
+ * This code was developed as part of the MediathekView project https://github.com/mediathekview/MediathekView
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package mediathek.gui.dialog;
 
+import mediathek.config.BuildInfo;
 import mediathek.config.Konstanten;
 import mediathek.gui.actions.UrlHyperlinkAction;
 import net.miginfocom.layout.AC;
@@ -21,7 +40,12 @@ public class AboutDialog extends JDialog {
         super(owner);
         initComponents();
 
-        lblVersion.setText(String.format("Version %s (%s)", Konstanten.MVVERSION, SystemUtils.OS_ARCH));
+        var buildInfo = BuildInfo.current();
+        var versionText = String.format("Version %s (%s)", Konstanten.MVVERSION, SystemUtils.OS_ARCH);
+        if (buildInfo.hasGitMetadata()) {
+            versionText = String.format("<html>%s<br/>Build: %s</html>", versionText, buildInfo.formatForDisplay());
+        }
+        lblVersion.setText(versionText);
 
         hyperlinkHomepage.addActionListener(_ -> UrlHyperlinkAction.openURL(Konstanten.ADRESSE_WEBSITE));
         hyperlinkGuiDonation.addActionListener(_ -> UrlHyperlinkAction.openURL("https://paypal.me/ChristianFranzke"));
