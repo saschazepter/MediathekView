6b4abe6a0a5b7f5635fa23d274300432eda56e8b
Christian F. <crystalpalace1977@googlemail.com>
2026-03-10 18:23:37 +0100

    update copyright

--- src/main/kotlin/mediathek/swingaudiothek/ui/AudiothekDetailsPanel.kt
+++ src/main/kotlin/mediathek/swingaudiothek/ui/AudiothekDetailsPanel.kt
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2026 derreisende77.
+ * This code was developed as part of the MediathekView project https://github.com/mediathekview/MediathekView
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package mediathek.swingaudiothek.ui
 
 import mediathek.swingaudiothek.model.AudioEntry
@@ -44,7 +62,7 @@ class AudiothekDetailsPanel : JPanel(BorderLayout(0, 8)) {
             detailsTitle.text = "Kein Eintrag ausgewählt"
             detailsMeta.text = " "
             websiteUrl = null
-            websiteLink.setClicked(false)
+            websiteLink.isClicked = false
             websiteLink.isVisible = false
             descriptionArea.text = ""
             return
@@ -74,7 +92,7 @@ class AudiothekDetailsPanel : JPanel(BorderLayout(0, 8)) {
             }
         }
         websiteUrl = entry.websiteUrl
-        websiteLink.setClicked(false)
+        websiteLink.isClicked = false
         websiteLink.isVisible = websiteUrl != null
         descriptionArea.text = entry.description.ifBlank { "Keine Beschreibung vorhanden." }
     }
