fd4d839219cb89d568cfc18a080b589fae0440cd
Christian F. <crystalpalace1977@googlemail.com>
2026-04-10 11:59:21 +0200

    wip

---
+++ src/main/kotlin/mediathek/gui/tabs/tab_downloads/FinishedDownloadsInfoLabel.kt
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2026 derreisende77.
+ * This code was developed as part of the MediathekView project https://github.com/mediathekview/MediathekView
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package mediathek.gui.tabs.tab_downloads
+
+import mediathek.daten.DownloadStartInfo
+import javax.swing.JLabel
+import javax.swing.SwingUtilities
+
+class FinishedDownloadsInfoLabel(startInfoProperty: DownloadStartInfoProperty) : JLabel() {
+    init {
+        toolTipText = "Anzahl der abgeschlossenen Downloads"
+        startInfoProperty.addStartInfoChangeListener { event ->
+            SwingUtilities.invokeLater {
+                process(event.newValue as DownloadStartInfo)
+            }
+        }
+    }
+
+    private fun process(info: DownloadStartInfo) {
+        text = if (info.hasValues()) {
+            if (info.finished == 1) "1 fertig" else "${info.finished} fertig"
+        } else {
+            "0 fertig"
+        }
+    }
+}
