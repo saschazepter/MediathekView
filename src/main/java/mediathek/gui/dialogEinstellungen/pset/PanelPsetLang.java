0ca963bee965561dbfd311f14e911ecf355e169a
Christian F. <crystalpalace1977@googlemail.com>
2026-05-27 12:13:55 +0200

    Narrow program cell renderer

--- src/main/java/mediathek/gui/dialogEinstellungen/pset/PanelPsetLang.java
+++ src/main/java/mediathek/gui/dialogEinstellungen/pset/PanelPsetLang.java
@@ -412,8 +412,6 @@ public class PanelPsetLang extends PanelVorlage {
                 }
             }
         });
-        tabelleProgramme.setDefaultRenderer(Object.class, new CellRendererProgramme());
-
         tabellePset.setDefaultRenderer(Object.class, new CellRendererPset());
         tabellePset.getSelectionModel().addListSelectionListener(event -> {
             if (!stopBeob) {
@@ -626,6 +624,7 @@ public class PanelPsetLang extends PanelVorlage {
                 default -> jRadioButtonAufloesungNormal.setSelected(true);
             }
             tabelleProgramme.setModel(pSet.getListeProg().createModel());
+            installProgrammeRenderers();
             if (tabelleProgramme.getRowCount() > 0) {
                 spaltenSetzenProgramme();
                 tabelleProgramme.setRowSelectionInterval(0, 0);
@@ -647,9 +646,17 @@ public class PanelPsetLang extends PanelVorlage {
             tfGruppeZielPfad.setText("");
             jTextAreaSetBeschreibung.setText("");
             tabelleProgramme.setModel(new NonEditableTableModel(new Object[0][DatenProg.MAX_ELEM], DatenProg.COLUMN_NAMES));
+            installProgrammeRenderers();
         }
         stopBeob = false;
         fillTextProgramme();
+    }
+
+    private void installProgrammeRenderers() {
+        var renderer = new CellRendererProgramme();
+        var columnModel = tabelleProgramme.getColumnModel();
+        columnModel.getColumn(tabelleProgramme.convertColumnIndexToView(DatenProg.PROGRAMM_RESTART)).setCellRenderer(renderer);
+        columnModel.getColumn(tabelleProgramme.convertColumnIndexToView(DatenProg.PROGRAMM_DOWNLOADMANAGER)).setCellRenderer(renderer);
     }
 
     public void spaltenSetzenProgramme() {
--- src/main/kotlin/mediathek/tool/cellrenderer/CellRendererProgramme.kt
+++ src/main/kotlin/mediathek/tool/cellrenderer/CellRendererProgramme.kt
@@ -1,8 +1,6 @@
 package mediathek.tool.cellrenderer
 
-import mediathek.daten.DatenProg
 import mediathek.tool.SVGIconUtilities
-import org.apache.logging.log4j.LogManager
 import java.awt.Component
 import javax.swing.JTable
 import javax.swing.SwingConstants
@@ -19,21 +17,13 @@ class CellRendererProgramme : DefaultTableCellRenderer() {
     ): Component {
         icon = null
         super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column)
-        try {
-            val modelColumn = table.convertColumnIndexToModel(column)
-            if (modelColumn == DatenProg.PROGRAMM_RESTART || modelColumn == DatenProg.PROGRAMM_DOWNLOADMANAGER) {
-                horizontalAlignment = SwingConstants.CENTER
-                icon = if (text == true.toString()) checkIcon else null
-                text = ""
-            }
-        } catch (ex: Exception) {
-            logger.error("getTableCellRendererComponent", ex)
-        }
+        horizontalAlignment = SwingConstants.CENTER
+        icon = if (text == true.toString()) checkIcon else null
+        text = ""
         return this
     }
 
     private companion object {
         private val checkIcon = SVGIconUtilities.createSVGIcon("icons/fontawesome/check.svg")
-        private val logger = LogManager.getLogger()
     }
 }
