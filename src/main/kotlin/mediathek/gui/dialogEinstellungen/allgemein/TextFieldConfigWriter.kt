3b752b8a0bd29016b3119ba8900b8cf9236ca979
Christian F. <crystalpalace1977@googlemail.com>
2026-06-08 09:33:32 +0200

    Convert timed document listener to Kotlin

--- src/main/kotlin/mediathek/gui/dialogEinstellungen/allgemein/TextFieldConfigWriter.kt
+++ src/main/kotlin/mediathek/gui/dialogEinstellungen/allgemein/TextFieldConfigWriter.kt
@@ -1,6 +1,8 @@
 package mediathek.gui.dialogEinstellungen.allgemein
 
 import mediathek.tool.ApplicationConfiguration
+import mediathek.tool.withLock
+import org.apache.commons.configuration2.sync.LockMode
 import java.awt.event.ActionEvent
 import java.awt.event.ActionListener
 import javax.swing.JTextField
@@ -10,6 +12,8 @@ class TextFieldConfigWriter(
     private val configPropertyKey: String,
 ) : ActionListener {
     override fun actionPerformed(e: ActionEvent) {
-        ApplicationConfiguration.getConfiguration().setProperty(configPropertyKey, control.text)
+        ApplicationConfiguration.getConfiguration().withLock(LockMode.WRITE) {
+            setProperty(configPropertyKey, control.text)
+        }
     }
 }
